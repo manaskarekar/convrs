93b164d7fd3cfed147ff60b05668f650361b7e20
Manas Karekar <powerslave12r@gmail.com>
2016-11-16 22:04:07 -0500

    Some cleanup

--- src/ir.rs
+++ src/ir.rs
@@ -27,40 +27,32 @@ pub struct IntermediateRepr {
 	foldLine3 : String,
 }
 
-pub fn ir_to_json(ir: String) {
+pub fn ir_to_json(ir: &IntermediateRepr) -> String {
+	let encoded = json::encode(&ir).unwrap();
+	println!("\nEncoded: {:#?}", &encoded);
 
+	encoded
 }
 
 pub fn json_to_ir(json_data: &String) -> IntermediateRepr {
 	let decoded: IntermediateRepr = json::decode(json_data).unwrap();
 	println!("\njson_data: {:#?}", &json_data);
 
-	return decoded;
+	decoded
 }
 
-pub fn write_json(ir_file: String) {}
+pub fn write_json(ir_file: String) {
+	//json file
+}
 
-pub fn read_json2(ir_file: String) -> String {
+pub fn read_json(ir_file: String) -> String {
 	println!("json file: {}", &ir_file);
 
 	let mut f = File::open(&ir_file).expect("Failed to read .ir file.");
 	let mut data = String::new();
 
 	f.read_to_string(&mut data).unwrap();
+	//let json_data = json::Json::from_str(&data).unwrap(); //returns json::Json object
 
 	data
-}
-
-pub fn read_json(ir_file: String) -> json::Json {
-	println!("json file: {}", &ir_file);
-
-	let mut f = File::open(&ir_file).expect("Failed to read .ir file.");
-	let mut data = String::new();
-
-	f.read_to_string(&mut data).unwrap();
-
-	let json_data = json::Json::from_str(&data).unwrap();
-	//println!("\ndata: {:#?}", json_data);
-
-	json_data
 }
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ mod ir;
 use ir::*;
 
 fn main() {
-	//println!("A tool to convert color schemes between editors.");
+	println!("A tool to convert color schemes between editors.");
 
-	//let json_data = ir::read_json2("src/test.json".to_string());
-	let json_data = ir::read_json2("src/test.json".to_string());
+	let json_data = ir::read_json("src/test.json".to_string());
 	//println!("{:#?}", &json_data);
 	let ir_obj = ir::json_to_ir(&json_data);
+	let json_string = ir_to_json(&ir_obj);
 	//let x:
 }
