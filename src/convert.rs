498348eb56fd35f4c93f3d7d8ba798775069b0d8
Manas Karekar <powerslave12r@gmail.com>
2016-12-01 06:54:34 -0500

    Added a message to print out list of supported file types.

--- src/convert.rs
+++ src/convert.rs
@@ -6,18 +6,20 @@ pub fn convert(source_file: &String, source_format: &String, dest_format: &Strin
 
 	let mut source_data: IntermediateRepr = Default::default();
 
+	let formats_available = [".vim" , "vim" , "vi" , "gvim" , ".gvim", ".jedit" , "jedit" , "jEdit", "paletton_txt"]; //make dict and use that below
+
 	match source_format.as_str() { //TODO: handle lowercase
 		".vim" | "vim" | "vi" | "gvim" | ".gvim" => source_data = vim::read_scheme(source_file),
 		".jedit" | "jedit" | "jEdit" => source_data = jedit::read_scheme(source_file),
 		"paletton_txt" => source_data = paletton_txt::read_scheme(source_file),
-		&_ => println!("Unknown source filetype."),		//TODO: How to handle this "None"?
+		&_ => println!("Error: Unknown source filetype. Choose one from: {:?}", &formats_available),		//TODO: How to handle this "None"?
 	};
 
 	match dest_format.as_str() {
 		".vim" | "vim" | "vi" | "gvim" | ".gvim" => vim::write_scheme(),
 		".jedit" | "jedit" | "jEdit" => jedit::write_scheme(&source_data),
 		"paletton_txt" => paletton_txt::write_scheme(),
-		&_ => println!("Unknown dest filetype."),
+		&_ => println!("Error: Unknown dest filetype. Choose one from: {:?}", &formats_available),
 	}
 	()
 }
