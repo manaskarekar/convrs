4e4843d9f928cf2176c3f9c5cdbd445df1ea7e14
Manas Karekar <powerslave12r@gmail.com>
2017-02-10 08:08:33 -0500

    Handle lower/upper case input for input/ouput format types

--- src/convert.rs
+++ src/convert.rs
@@ -6,23 +6,23 @@ pub fn convert(source_file: &String, source_format: &String, dest_format: &Strin
 
 	let mut source_data: IntermediateRepr = Default::default();
 
-	let formats_available = [".vim" , "vim" , "vi" , "gvim" , ".gvim", ".jedit" , "jedit" , "jEdit", "paletton_txt"]; //make dict and use that below
+	let formats_available = [".vim" , "vim" , "vi" , "gvim" , ".gvim", ".jedit" , "jedit", "paletton_txt", "tmtheme", "tm"]; //make dict and use that below
 
-	match source_format.as_str() { //TODO: handle lowercase
+	match source_format.to_lowercase().as_str() {
 		".vim" | "vim" | "vi" | "gvim" | ".gvim" => source_data = vim::read_scheme(source_file),
-		".jedit" | "jedit" | "jEdit" => source_data = jedit::read_scheme(source_file),
+		".jedit" | "jedit" => source_data = jedit::read_scheme(source_file),
 		"paletton_txt" => source_data = paletton_txt::read_scheme(source_file),
 		"sublime-theme" | "subl" | "sublimetext" => source_data = st3::read_scheme(source_file),
-		"tmTheme" | "tm" => source_data = tm::read_scheme(source_file),
+		"tmtheme" | "tm" => source_data = tm::read_scheme(source_file),
 		&_ => println!("Error: Unknown source filetype. Choose one from: {:?}", &formats_available),		//TODO: How to handle this "None"?
 	};
 
-	match dest_format.as_str() {
+	match dest_format.to_lowercase().as_str() {
 		".vim" | "vim" | "vi" | "gvim" | ".gvim" => vim::write_scheme(),
-		".jedit" | "jedit" | "jEdit" => jedit::write_scheme(&source_data),
+		".jedit" | "jedit" => jedit::write_scheme(&source_data),
 		"paletton_txt" => paletton_txt::write_scheme(),
 		"sublime-theme" | "subl" | "sublimetext" => st3::write_scheme(),
-		"tmTheme" | "tm" => tm::write_scheme(),
+		"tmtheme" | "tm" => tm::write_scheme(),
 		&_ => println!("Error: Unknown dest filetype. Choose one from: {:?}", &formats_available),
 	}
 }
